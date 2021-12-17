fa034b05021295544b79e6583e3c7c5f404be0a5
Arthur <aldmb14@gmail.com>
2021-12-18 00:32:16 +0500

    first commit

---
+++ app/Http/Controllers/Cover.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Cover extends Controller
+{
+   public function index(){
+       return 'This is Cover controller';
+   }
+}
